de58d5da85a82d9f487295b136b46f76b23b4945
MHTB <mhtb32@gmail.com>
2018-10-12 16:03:43 +0330

    License changed

--- moving_average.h
+++ moving_average.h
@@ -6,7 +6,7 @@
   ******************************************************************************
   *MIT License
 	*
-	*Copyright (c) 2018 [Atrovan]
+	*Copyright (c) 2018 Mohammad Hussein Tavakoli Bina 
 	*
 	*Permission is hereby granted, free of charge, to any person obtaining a copy
 	*of this software and associated documentation files (the "Software"), to deal
